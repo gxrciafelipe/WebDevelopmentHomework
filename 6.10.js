df87ed691e6cc1e089313bc2f1886fe4fa38438c
Felipe Garcia <felipesgarcia17@gmail.com>
2025-11-07 17:29:31 -0500

    New homework

---
+++ 6.10.js
@@ -0,0 +1,32 @@
+let musicQueue = {
+   songs: ["Party Rock Anthem", "I Gotta Feeling", "Macarena"],
+   nextSong: 0,
+
+   get next() {
+      tmp = this.nextSong;
+      this.nextSong++;
+      if (this.nextSong < 0 || this.nextSong > this.songs.length-1){
+         this.nextSong = 0;
+      }   
+      return this.songs[tmp];
+   },
+    set next(index) {
+      this.nextSong = index;
+      if ( this.nextSong < 0 || this.nextSong > this.songs.length-1){
+         this.nextSong = 0;
+      }
+   }
+};
+
+// Run through the queue three times
+for (let c = 0; c < musicQueue.songs.length * 3; c++) {
+   console.log("Now playing: " + musicQueue.next);
+}
+
+// Test the next setter
+musicQueue.next = 2;
+console.log(musicQueue.next);   // Macarena
+musicQueue.next = 3;
+console.log(musicQueue.next);   // Party Rock Anthem
+musicQueue.next = -1;
+console.log(musicQueue.next);   // Party Rock Anthem
